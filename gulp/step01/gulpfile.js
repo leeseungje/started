33b9c649a75df00e759854f0e246408a70cd637b
leeseungje <leeseungje@naver.com>
2016-03-04 13:29:33 +0900

    예제 수정

--- gulp/step01/gulpfile.js
+++ gulp/step01/gulpfile.js
@@ -1,8 +1,17 @@
 /**
  * Created by 이승제 on 2016-03-03.
  */
-var gulp = require("gulp");
+var gulp = require("gulp"),
+    sass = require("gulp-sass");
 
+// 예제 1
  gulp.task("default", function() {
      console.log("Hello world")
  })
+
+// 예제 2
+gulp.task('compile-sass', function () {
+    return gulp.src("./sass/**/*.sass")
+        .pipe(sass())
+        .pipe(gulp.dest("./css"));
+});
